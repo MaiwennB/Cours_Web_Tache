af9c73bb6a83003cb08726ae69a91c8cecd75cbe
FremontJoshua <fremont.joshua85@gmail.com>
2018-01-08 10:41:37 +0100

    valid

--- 1.0/index2.php
+++ 1.0/index2.php
@@ -1,4 +1,4 @@
-	<?php
+<?php
 		require_once 'vendor/autoload.php';
 		// Variables de connection à la BDD
 		$hostname = "localhost";
@@ -16,24 +16,11 @@
 			echo $e->getMessage();
 		}
 
-		// Requette de selection
-		$req = " CREATE TABLE IF NOT EXISTS `tache` (`id` int(11) NOT NULL,
-	  												`libelle` varchar(340) NOT NULL)
-				ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;";
-		$taches = $bdd->query($req);
-		// Requette de selection
-		$req = "SELECT * FROM tache ORDER BY id";
-		$taches = $bdd->query($req);
-		
-		$m = new Mustache_Engine(array(
-			'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views'),
-		));
-		echo $m->render("liste", array('taches'=> $taches));
-
+	
 
 
 		// AJOUT
-		/* if (isset ($_POST['valider']))
+		if (isset ($_POST['valider']))
 		{
 			$newTask = $_POST['libTache'];
 			// Si le champ est vide, lancer une erreur
@@ -57,23 +44,6 @@
 					echo$info = "Erreur lors de l'insertion";
 				}	
 		  	}
-		} */
-
-		// SUPPRESSION
-		if (isset ($_POST['id']))
-		{
-		  	$tasksuppr = $_POST['id'];
-			// Insertion dans la bdd
-			$query = ("DELETE FROM tache WHERE id=(:tasksuppr)");
-			$stmt = $bdd->prepare($query);
-			$stmt->bindParam(':tasksuppr', $tasksuppr);
-			if ($stmt->execute())
-			{
-				echo $info = 'Suppression réussi';
-				header('Location: index.php');
-			}
-			else
-			{
-			echo $info = 'Erreur lors de la création de la tâche';
-			}
 		}
+
+		
